69842869efb7071bb0e8c195dcb2358cf1eb825d
Artem Konyshev <konyshev_arte@mail.ru>
2019-09-14 20:23:42 +0500

    Update spawner; Remove Saved folder; Add materials
    - Add FSpawnerBounds struct that contains references  to bound mesh components of spawner
    -  Add width, height and depth properties to BoidSpawner class
    - Add GetSpawnerBounds method to BoidSpawner class that returns FSpawnerBounds struct containing all spawner bounds
    - Add UpdateBounds method to BoidSpawner class that updates position and location of all found spawner bounds
    - Add UpdateBound method to BoidSpawner class that updates position and location of one bound
    - Override OnConstruction method in BoidSpawner that gets called when properties of blueprint get changed
    - Remove Saved folder from version control
    - Add translucent SpawnerBound material
    - Add Boid material

--- Source/UnrealBoids/BoidSpawner.cpp
+++ Source/UnrealBoids/BoidSpawner.cpp
@@ -4,13 +4,21 @@
 #include "BoidSpawner.h"
 #include "Engine/World.h"
 #include "BoidPawn.h"
+#include "Engine/Engine.h"
 
 // Sets default values
 ABoidSpawner::ABoidSpawner()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+}
 
+void ABoidSpawner::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	const FSpawnerBounds bounds = GetSpawnerBounds();
+	UpdateBounds(bounds);
 }
 
 // Called when the game starts or when spawned
@@ -19,13 +27,12 @@ void ABoidSpawner::BeginPlay()
 	Super::BeginPlay();
 
 	if (!BoidBlueprintType) return;
-	
+
 	for (int i = 0; i < TotalBoids; ++i)
 	{
 		SpawnBoid();
 	}
 }
-
 
 void ABoidSpawner::SpawnBoid() const
 {
@@ -33,5 +40,60 @@ void ABoidSpawner::SpawnBoid() const
 	parameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 	const FVector direction = FMath::VRand();
 	GetWorld()->SpawnActor<ABoidPawn>(BoidBlueprintType.Get(), GetActorLocation(), direction.Rotation(), parameters);
+}
+
+/**
+ * Get all bounds of spawner and put them in struct.
+ *
+ * @return struct containing all bounds of spawner
+ */
+FSpawnerBounds ABoidSpawner::GetSpawnerBounds() const
+{
+	TArray<UActorComponent*> components = GetComponentsByClass(UStaticMeshComponent::StaticClass());
+	TMap<FString, UStaticMeshComponent*> nameToMesh = TMap<FString, UStaticMeshComponent*>();
+	for (UActorComponent* component : components)
+	{
+		UStaticMeshComponent* mesh = Cast<UStaticMeshComponent>(component);
+		nameToMesh.Add(mesh->GetName(), mesh);
+	}
+	return FSpawnerBounds(nameToMesh.FindRef("LeftBound"), nameToMesh.FindRef("RightBound"), nameToMesh.FindRef("BackBound"),
+		nameToMesh.FindRef("FrontBound"), nameToMesh.FindRef("TopBound"), nameToMesh.FindRef("BottomBound"));
+}
+
+/**
+ * Update location and scale of all bounds based on exposed properties.
+ *
+ * @param bounds Struct containing all bounds of spawner.
+ */
+void ABoidSpawner::UpdateBounds(FSpawnerBounds bounds) const
+{
+	const FVector yLocation = FVector(0, -BoidAreaWidth / 2, 0);
+	const FVector yScale = FVector(BoidAreaDepth / 100, 1, BoidAreaHeight / 100);
+	UpdateBound(bounds.LeftBound, yLocation, yScale);
+	UpdateBound(bounds.RightBound, -yLocation, yScale);
+
+	const FVector xLocation = FVector(BoidAreaDepth / 2, 0, 0);
+	const FVector xScale = FVector(1, BoidAreaWidth / 100, BoidAreaHeight / 100);
+	UpdateBound(bounds.FrontBound, xLocation, xScale);
+	UpdateBound(bounds.BackBound, -xLocation, xScale);
+
+	const FVector zLocation = FVector(0, 0, BoidAreaHeight / 2);
+	const FVector zScale = FVector(BoidAreaDepth / 100, BoidAreaWidth / 100, 1);
+	UpdateBound(bounds.TopBound, zLocation, zScale);
+	UpdateBound(bounds.BottomBound, -zLocation, zScale);
+}
+
+/**
+ * Update bound location and scale.
+ *
+ * @param bound Reference to bound that will be updated.
+ * @param location New relative location of bound.
+ * @param scale New relative scale of bound.
+ */
+void ABoidSpawner::UpdateBound(UStaticMeshComponent* bound, FVector location, FVector scale) const
+{
+	if (!bound) return;
+	bound->SetRelativeLocation(location);
+	bound->SetRelativeScale3D(scale);
 }
 
--- Source/UnrealBoids/BoidSpawner.h
+++ Source/UnrealBoids/BoidSpawner.h
@@ -4,7 +4,44 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 #include "BoidSpawner.generated.h"
+
+USTRUCT()
+struct UNREALBOIDS_API FSpawnerBounds
+{
+	GENERATED_BODY()
+
+public:
+	FSpawnerBounds() = default;
+	FSpawnerBounds(UStaticMeshComponent* leftBound, UStaticMeshComponent* rightBound, UStaticMeshComponent* backBound,
+		UStaticMeshComponent* frontBound, UStaticMeshComponent* topBound, UStaticMeshComponent* bottomBound)
+	{
+		LeftBound = leftBound;
+		RightBound = rightBound;
+		BackBound = backBound;
+		FrontBound = frontBound;
+		TopBound = topBound;
+		BottomBound = bottomBound;
+	}
+	virtual ~FSpawnerBounds() = default;
+
+	UPROPERTY()
+		UStaticMeshComponent* LeftBound;
+	UPROPERTY()
+		UStaticMeshComponent* RightBound;
+	UPROPERTY()
+		UStaticMeshComponent* BackBound;
+	UPROPERTY()
+		UStaticMeshComponent* FrontBound;
+	UPROPERTY()
+		UStaticMeshComponent* TopBound;
+	UPROPERTY()
+		UStaticMeshComponent* BottomBound;
+};
+
+
+
 
 UCLASS()
 class UNREALBOIDS_API ABoidSpawner : public AActor
@@ -18,6 +55,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boid spawner properties")
@@ -25,6 +63,16 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boid spawner properties")
 		int TotalBoids;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boid spawner properties", meta = (ClipMin = "100"))
+		float BoidAreaWidth;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boid spawner properties", meta = (ClipMin = "100"))
+		float BoidAreaHeight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boid spawner properties", meta = (ClipMin = "100"))
+		float BoidAreaDepth;
+
 private:
 	void SpawnBoid() const;
+	FSpawnerBounds GetSpawnerBounds() const;
+	void UpdateBounds(FSpawnerBounds bounds) const;
+	void UpdateBound(UStaticMeshComponent* bound, FVector location, FVector scale) const;
 };
